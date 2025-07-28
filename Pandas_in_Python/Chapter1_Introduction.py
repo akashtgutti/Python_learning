ef13888c054af7ad1aefadffe68b647a1a693786
akashtgutti <akashgutti3@gmail.com>
2025-07-28 16:29:45 +0530

    Basic understanding of Pandas library in python

---
+++ Pandas_in_Python/Chapter1_Introduction.py
@@ -0,0 +1,30 @@
+########################## Data Analysis ##########################
+
+# Data analysis is a process of inspecting, cleansing, transforming, and modelling data with the goal of discovering
+# useful information, informing conclusions, and supporting decision-making.
+
+
+########################## What is Pandas?  ##########################
+
+# The name "pandas" has a reference to both "panel data", and "python data analysis" and was created by wes mckinney in 2008.
+# Pandas is a python library used for working with data sets.
+# It has functions for analyzing, cleaning, exploring, and manipulating data.
+# Read and write data structures and different formats : csv, text, XML,JSON,ZIP etc.
+
+
+########################## Pandas Data Structures ##########################
+
+# Three Data Structures:
+# Series: One-Dimensional labeled arrays pd.Series(data)
+# DataFrames: Two-Dimensional data structure with columns, much like a table.
+# Panel : A panel is a 3D container of data.
+
+
+########################## Importance of Pandas in Python ##########################
+
+# Pandas allows us to analyze big data and make conclusions based on statistical theories.
+# Pandas can clean messy data sets, and make them readable and relevant.
+# Easy handling of missing data (represented as NaN) in floating point as well as non-floating point data.
+
+# Size mutability: columns can be inserted and deleted from DataFrame and higher dimensional objects.
+# > Data set merging and joining. Flexible reshaping and pivoting of data sets Provide series functionality.
