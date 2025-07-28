21ed3640d4ba3c9a76616527b0e4d799dda54263
akashtgutti <akashgutti3@gmail.com>
2025-07-28 11:10:41 +0530

    Dictionary and Nested dictionary in python

--- Python_Basics/Dictionary_in_python.py
+++ Python_Basics/Dictionary_in_python.py
@@ -41,3 +41,38 @@ thisdict = {
 }
 thisdict.popitem()
 print(thisdict)
+
+# Update the "year" of the car by using the update() method:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"year": 2020})
+
+print(thisdict)
+
+# Adding Items
+# Adding an item to the dictionary is done by using a new index key and assigning a value to it:
+
+thisdict2 = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict2["color"] = "red"
+print(thisdict2)
+
+# Update Dictionary
+# The update() method will update the dictionary with the items from a given argument. 
+# If the item does not exist, the item will be added.
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"chassi no":"MS-12-45-67"})
+
+print(thisdict)
--- Python_Basics/nested_dictionary.py
+++ Python_Basics/nested_dictionary.py
@@ -19,3 +19,23 @@ myfamily = {
 
 
 print(myfamily["child3"]["year"])
+
+# Loop Dictionary
+
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict:
+  print(x)
+  
+  
+for x in thisdict.values():
+  print(x)
+  
+
+# Loop through both keys and values, by using the items() method:
+
+for x, y in thisdict.items():
+  print(x, y)
