2308fee1b44a1c713949a7ef6fb298cbcbb97028
akashtgutti <akashgutti3@gmail.com>
2025-07-29 17:51:39 +0530

    delete in pandas

---
+++ Pandas_in_Python/Delete_insert_data_in_python.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+x = {"A":[1,2,3,4,5],"B":["Akash","Gutti","T","Kumar","Reddy"],"C":[10,20,30,40,50]}
+
+df = pd.DataFrame(x)
+print(df)
+
+#Insert column
+df.insert(1, "D","B")
+print(df)
+
+#Duplicate copy of the column
+
+df.insert(1, "D1", df["B"])
+print(df)
+
+df["D2"] = df["B"][:4]
+
+print(df)
+
+#Delete column
+var = df.pop("D2")
+
+print(df)
+print(var)
