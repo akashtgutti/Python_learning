6bcf62c182756fff011e7f9352df0298da1d26e8
akashtgutti <akashgutti3@gmail.com>
2025-11-27 18:19:28 +0530

    class method

---
+++ Oop_in_python/Python_class_method.py
@@ -0,0 +1,28 @@
+# Class Methods
+# Methods are functions that belong to a class. They define the behavior of objects created from the class.
+
+class Bank:
+    def __init__(self,account_type, bank_name):
+        self.bank_name = bank_name
+        self.account_type = account_type
+        
+    def bank_address(self):
+        print("PCMC,pune,Maharashtra,India")
+        
+p1 = Bank("savings","SBI")
+print(p1.bank_address())
+
+# Methods with Parameters
+# Methods can accept parameters just like regular functions:
+
+
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def multiply(self, a, b):
+        return a * b
+
+calc = Calculator()
+print(calc.add(5, 3))
+print(calc.multiply(4, 7))
