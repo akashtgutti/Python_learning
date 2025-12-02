4c7fafce1b4dea1bfb382f43ba8ee47476cf533c
akashtgutti <akashgutti3@gmail.com>
2025-12-02 21:33:30 +0530

    Getter and Setter in python

---
+++ Oop_in_python/Getter_and_Setter_in_python.py
@@ -0,0 +1,11 @@
+# Getter and Setter in Python
+
+# In Python, a getter and setter are methods used to access and update the attributes of a class. 
+# These methods provide a way to define controlled access to the attributes of an object, thereby ensuring the integrity of the data. 
+# By default, attributes in Python can be accessed directly. However, this can pose problems when attributes need validation or 
+# transformation before being assigned or retrieved.
+
+# Getter: The getter method is used to retrieve the value of a private attribute. It allows controlled access to the attribute.
+# Setter: The setter method is used to set or modify the value of a private attribute. It allows you to control how the value is 
+# updated, enabling validation or modification of the data before it’s actually assigned.
+
