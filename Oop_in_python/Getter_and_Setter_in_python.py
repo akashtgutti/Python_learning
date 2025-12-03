c685fa6d931cb9077a7662a6dd9754f9ba05f4fe
akashtgutti <akashgutti3@gmail.com>
2025-12-03 23:37:06 +0530

    Getters code updated

--- Oop_in_python/Getter_and_Setter_in_python.py
+++ Oop_in_python/Getter_and_Setter_in_python.py
@@ -11,18 +11,20 @@
 
 
 #Setters
+
 class MyClass:
-    def _init_(self, value):
-      self._value = value
+    def __init__(self, value):
+        self._value = value
 
     def show(self):
-      print(f"Value is {self._value}")
+        print(f"Value is {self._value}")
 
     @property
     def ten_value(self):
-        return 10* self ._value
+        return 10 * self._value
+
 
 obj = MyClass(10)
-obj.ten_value = 67
 print(obj.ten_value)
-obj. show( )
+obj.show()
+
