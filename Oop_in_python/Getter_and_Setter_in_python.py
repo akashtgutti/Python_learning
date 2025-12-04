0fa1097a58dc629fcfce7472dac9007d8557554f
akashtgutti <akashgutti3@gmail.com>
2025-12-04 22:07:47 +0530

    Gettters and setters code updated using normal function and using property function

--- Oop_in_python/Getter_and_Setter_in_python.py
+++ Oop_in_python/Getter_and_Setter_in_python.py
@@ -10,21 +10,55 @@
 # updated, enabling validation or modification of the data before it’s actually assigned.
 
 
-#Setters
+#Getters and setters using normal method
 
-class MyClass:
-    def __init__(self, value):
-        self._value = value
+class Geek: 
+	def __init__(self, age = 0): 
+		self._age = age 
+	
+	# getter method 
+	def get_age(self): 
+		return self._age 
+	
+	# setter method 
+	def set_age(self, x): 
+		self._age = x 
 
-    def show(self):
-        print(f"Value is {self._value}")
+raj = Geek() 
 
-    @property
-    def ten_value(self):
-        return 10 * self._value
+# setting the age using setter 
+raj.set_age(21) 
 
+# retrieving age using getter 
+print(raj.get_age()) 
 
-obj = MyClass(10)
-print(obj.ten_value)
-obj.show()
+print(raj._age)
+
+#Getters and setters using property() function
+
+class Geeks: 
+	def __init__(self): 
+		self._age = 0
+	
+	# function to get value of _age 
+	def get_age(self): 
+		print("getter method called") 
+		return self._age 
+	
+	# function to set value of _age 
+	def set_age(self, a): 
+		print("setter method called") 
+		self._age = a 
+
+	# function to delete _age attribute 
+	def del_age(self): 
+		del self._age 
+	
+	age = property(get_age, set_age, del_age) 
+
+mark = Geeks() 
+
+mark.age = 10
+
+print(mark.age)
 
