05deb082af2ce778223e6ba93606ded5564062f8
akashtgutti <akashgutti3@gmail.com>
2025-12-06 22:58:47 +0530

    Polymorphism in python

---
+++ Oop_in_python/Polymorphism_in_python.py
@@ -0,0 +1,13 @@
+# What is Polymorphism?
+# The literal meaning of polymorphism is the condition of occurrence in different forms.
+# Polymorphism is a very important concept in programming. It refers to the use of a single type entity
+# (method, operator or object) to represent different types in different scenarios.
+
+# Example 1: Polymorphism in addition operator
+# We know that the + operator is used extensively in Python programs. But, it does not have a single usage.
+
+# For integer data types, + operator is used to perform arithmetic addition operation.
+
+num1 = 1
+num2 = 2
+print(num1+num2)
