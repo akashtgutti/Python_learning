a0ff28755c0c197d5dffc41b66058cb79fed8c6d
akashtgutti <akashgutti3@gmail.com>
2025-12-31 15:00:21 +0530

    reshaping of array

---
+++ NumPy_in_python/chapter8_array_reshaping_in_numpy.py
@@ -0,0 +1,15 @@
+# Reshaping arrays
+# Reshaping means changing the shape of an array.
+
+# The shape of an array is the number of elements in each dimension.
+
+# By reshaping we can add or remove dimensions or change number of elements in each dimension.
+
+import numpy as np
+
+# Reshape From 1-D to 2-D
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+newarr = arr.reshape(4, 3)
+print(newarr)
+
