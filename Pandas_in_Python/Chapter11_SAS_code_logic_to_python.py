19847b0904cebe90b18799635753f2878f4dfbda
akashtgutti <akashgutti3@gmail.com>
2026-01-22 17:28:32 +0530

    learning  pd.merge like sql join

--- Pandas_in_Python/Chapter11_SAS_code_logic_to_python.py
+++ Pandas_in_Python/Chapter11_SAS_code_logic_to_python.py
@@ -22,3 +22,26 @@ print(df.head())
 print(df.info())
 df['high'] = np.where(df['exposure_amount'] > 10000,1,0)
 print(df['high'].head())
+
+
+# In Pandas, we can join two datasets using the merge() function, similar to SQL joins. 
+# We can perform inner, left, right, or outer joins depending on whether we want all records or only matching records
+
+policy = pd.DataFrame({
+    'policy_id': [101, 102, 103],
+    'member_name': ['Alice', 'Bob', 'Charlie']
+})
+
+claims = pd.DataFrame({
+    'claim_id': [1001, 1002, 1003],
+    'policy_id': [101, 103, 104],
+    'claim_amount': [5000, 7000, 2000]
+})
+
+# Inner join on policy_id
+merged_data = pd.merge(claims, policy, on='policy_id', how='inner')
+merged_data1 = pd.merge(claims, policy, on='policy_id', how='left')
+merged_data2 = pd.merge(claims, policy, on='policy_id', how='right')
+print(merged_data)
+print(merged_data1)
+print(merged_data2)
