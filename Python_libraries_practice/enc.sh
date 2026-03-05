b142fa698d138ef72a3d14368751b7f6ea0d809b
akashtgutti <akashgutti3@gmail.com>
2026-03-05 16:48:34 +0530

    enc.sh file added

---
+++ Python_libraries_practice/enc.sh
@@ -0,0 +1,9 @@
+echo "removing existing files from edip"
+
+rm -f C:/ATG_python_with_ETL/24_11_2025/Python_learning/Python_libraries_practice/sample_folder/b.txt
+rm -f C:/ATG_python_with_ETL/24_11_2025/Python_learning/Python_libraries_practice/sample_folder/s.txt
+
+echo "creating edip folders"
+
+mkdir -p C:/ATG_python_with_ETL/24_11_2025/Python_learning/Python_libraries_practice/sample_folder/edip
+mkdir -p C:/ATG_python_with_ETL/24_11_2025/Python_learning/Python_libraries_practice/sample_folder/edip/input
