b0ad8cdd536e85463bfc3795842d0d54b282c9aa
akashtgutti <akashgutti3@gmail.com>
2026-03-05 17:50:18 +0530

    enc.sh files path updated

--- Python_libraries_practice/enc.sh
+++ Python_libraries_practice/enc.sh
@@ -1,9 +1,9 @@
-echo "removing existing files from edip"
+echo "removing existing files from sample_folder"
 
 rm -f C:/ATG_python_with_ETL/24_11_2025/Python_learning/Python_libraries_practice/sample_folder/b.txt
 rm -f C:/ATG_python_with_ETL/24_11_2025/Python_learning/Python_libraries_practice/sample_folder/s.txt
 
-echo "creating edip folders"
+echo "creating akash folders"
 
-mkdir -p C:/ATG_python_with_ETL/24_11_2025/Python_learning/Python_libraries_practice/sample_folder/edip
-mkdir -p C:/ATG_python_with_ETL/24_11_2025/Python_learning/Python_libraries_practice/sample_folder/edip/input
+mkdir -p C:/ATG_python_with_ETL/24_11_2025/Python_learning/Python_libraries_practice/sample_folder/akash
+mkdir -p C:/ATG_python_with_ETL/24_11_2025/Python_learning/Python_libraries_practice/sample_folder/akash/input
